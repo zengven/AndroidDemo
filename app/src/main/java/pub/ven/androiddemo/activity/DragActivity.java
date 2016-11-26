c9476de930df6fdca10ab8858dd9cc1dd73aa2bb
zengven <ven200@163.com>
2016-11-26 14:11:12 +0800

    fix conflict

--- app/src/main/java/pub/ven/androiddemo/activity/DragActivity.java
+++ app/src/main/java/pub/ven/androiddemo/activity/DragActivity.java
@@ -1,7 +1,8 @@
 package pub.ven.androiddemo.activity;
 
 import android.os.Bundle;
-<<<<<<< HEAD
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,50 +11,18 @@ import pub.ven.androiddemo.R;
 public class DragActivity extends AppCompatActivity {
 
     private SwipeRefreshLayout mRefresh;
-=======
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
-
-import pub.ven.androiddemo.R;
-import pub.ven.androiddemo.widget.SwipeRefreshToLayout;
-
-public class DragActivity extends AppCompatActivity {
-
-    private SwipeRefreshToLayout mrefresh;
-    private Handler mHandler=new Handler(){
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            mrefresh.onRefreshComplete();
         }
     };
->>>>>>> 91bba06be752afcf84ab1b65d429aa3a38205c45
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag);
-<<<<<<< HEAD
         mRefresh = (SwipeRefreshLayout) findViewById(R.id.activity_drag);
-        mRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mRefresh.setRefreshing(false);
-=======
-        mrefresh = (SwipeRefreshToLayout) findViewById(R.id.activity_drag);
-        mrefresh.setMode(SwipeRefreshToLayout.Mode.PullUp);
-        mrefresh.setOnRefreshListener(new SwipeRefreshToLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mHandler.sendEmptyMessageDelayed(1,2000);
-            }
 
-            @Override
-            public void onLoadMore() {
-                mHandler.sendEmptyMessageDelayed(1,2000);
->>>>>>> 91bba06be752afcf84ab1b65d429aa3a38205c45
-            }
-        });
     }
 }
